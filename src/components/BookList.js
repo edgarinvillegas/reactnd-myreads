ff03ea72af03524a0af971f80d98c59bc1e27066
edgarinvillegas <edgarinvillegas@hotmail.com>
2018-04-03 12:01:32 -0400

    feat: BookList component now stable with props. Add proptypes package

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Book from './Book';
 import './BookList.css';
 
-const BookList = () => {
+const BookList = ({ shelfTitle = null, books }) => {
+
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">Currently Reading</h2>
+      {shelfTitle && <h2 className="bookshelf-title">{shelfTitle}</h2>}
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <li>
-            <Book />
-          </li>
+          {books.map( book => (
+            <li>
+              <Book book={book}/>
+            </li>
+          ))}
         </ol>
       </div>
     </div>
   );
 };
 
+BookList.propTypes = {
+  shelfTitle: PropTypes.string,
+  books: PropTypes.array.isRequired   //TODO: Consider creating a class/type Book
+};
+
 export default BookList;
+
+
